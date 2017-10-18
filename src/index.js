d092f70c587af07132bb39f2458d76451f8a9ccc
r.burdeiniy <roman.burdeiniy@gmail.com>
2017-10-18 14:09:02 +0300

    use new method from submodule

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ var core = require('./core/src/index');
 
 const result1 = core.mathUtils.add(2, 3);
 const result2 = core.mathUtils.multiply(2, 3);
+const result3 = core.mathUtils.subs(2, 3);
 
 console.log(result1);
 console.log(result2);
+console.log(result3);
